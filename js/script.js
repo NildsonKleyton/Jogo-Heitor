1cd0375981470124142e69d4e3bc608d7c376ecd
NildsonKleyton <nildsonkleyton@hotmail.com>
2023-04-23 22:55:13 -0300

    troca de nome

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
-const play = document.querySelector('.play');
+const player = document.querySelector('.player');
 const pipe = document.querySelector('.pipe');
 const terrain = document.querySelector('.terrain');
 const imgReload = document.querySelector('#btn-reload');
 const btnReload = document.querySelector('#btn-reload');
 const jump = () => {
-    play.classList.add('jump')
+    player.classList.add('jump')
     setTimeout(()=>{
-        play.classList.remove('jump')
+        player.classList.remove('jump')
     },500)
 }
 
 const loop = setInterval(() =>{
     const pipePosition = pipe.offsetLeft;
-    const playPosition = +window.getComputedStyle(play).bottom.replace('px', '');
+    const playerPosition = +window.getComputedStyle(player).bottom.replace('px', '');
     const terrainPosition  = terrain.offsetLeft;
 
-    if(pipePosition <= 85 && pipePosition > 0 && playPosition <165){
+    if(pipePosition <= 85 && pipePosition > 0 && playerPosition <165){
         
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
         
-        play.style.animation = 'none';
-        play.style.bottom = `${playPosition}px`;
+        player.style.animation = 'none';
+        player.style.bottom = `${playerPosition}px`;
 
-        play.src='./Images/playOver.png';
-        play.style.width = '6.5em';
-        play.style.marginLeft = '-5px'; 
-        play.style.marginBottom = '-5px';
+        player.src='./Images/playerOver.png';
+        player.style.width = '6.5em';
+        player.style.marginLeft = '-5px'; 
+        player.style.marginBottom = '-5px';
 
         imgReload.src='./Images/recaregar.png';
 
